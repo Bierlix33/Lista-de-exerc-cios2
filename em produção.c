582068a8c096e2fbee4820baecbbd3eda80d3f5a
Bierlix33 <Allangab21@gmail.com>
2025-05-22 00:50:33 -0300

    Create em produção.c

---
+++ em produção.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+
+int n1, n2, resultado;
+char operador;
+
+printf("digite um operador\n");
+    scanf(" %c", &operador);
+
+    printf("digite um número\n");
+    scanf("%d", &n1);
+
+    printf("digite outro número\n");
+    scanf("%d", &n2);
+
+
+switch (operador){
+    case '+':
+    resultado = (n1 + n2);
+    break;
+    
+    case '-':
+    resultado = (n1 - n2);
+    break;
+    case '*':
+    resultado = (n1 * n2);
+    break;
+    case '/':
+    resultado = (n1 / n2);
+    break;
+}
+printf("o resultado é %d\n", resultado);
+       
+    return 0
+    
+}
